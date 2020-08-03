91eabf71e9daf62cc7e4d0143bb2dfe46101a90c
bpurushoth <68917535+bpurushoth@users.noreply.github.com>
2020-08-03 20:02:59 +0530

    Update MavenTest1.java

--- MavenProject/src/test/java/testpackage/MavenTest1.java
+++ MavenProject/src/test/java/testpackage/MavenTest1.java
@@ -3,6 +3,7 @@ import static org.testng.AssertJUnit.assertEquals;
 import java.util.concurrent.TimeUnit;  
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;  
 import org.testng.annotations.BeforeTest;  
