d897fa091d06cb554c0d5f58cb368e96f015329c
bpurushoth <68917535+bpurushoth@users.noreply.github.com>
2020-08-03 19:51:17 +0530

    Update MavenTest1.java

--- MavenProject/src/test/java/testpackage/MavenTest1.java
+++ MavenProject/src/test/java/testpackage/MavenTest1.java
@@ -9,14 +9,18 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 public class MavenTest1 {  
 public String baseUrl = "https://www.google.com/";  
-String driverPath = "chromedriver.exe";  
+String driverPath = "/usr/bin/chromedriver";  
 public WebDriver driver ;   
 @Test             
 public void test() {      
 // set the system property for Chrome driver      
 System.setProperty("webdriver.chrome.driver", driverPath);  
 // Create driver object for CHROME browser  
-driver = new ChromeDriver();  
+ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+ 
+                WebDriver driver = new ChromeDriver(chromeOptions);  
 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
 driver.manage().window().maximize();  
 driver.get(baseUrl);  
