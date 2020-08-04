aa17da552b2612cf8f144c61376f3fc814e9c28e
bpurushoth <68917535+bpurushoth@users.noreply.github.com>
2020-08-04 22:42:23 +0530

    Update MavenTest1.java

--- MavenProject/src/test/java/testpackage/MavenTest1.java
+++ MavenProject/src/test/java/testpackage/MavenTest1.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;  
 import org.testng.annotations.BeforeTest;  
-import org.testng.annotations.Test;
+import org.testng.annotations.Test;  
 public class MavenTest1 {  
-public String baseUrl = "https://www.google.com/";  
+public String baseUrl1 = "https://www.google.com/";
+public String baseUrl2 = "https://twingo.techmahindra.com/"; 
 String driverPath = "/usr/bin/chromedriver";  
 public WebDriver driver ;   
 @Test             
@@ -18,22 +19,48 @@ public void test() {
 System.setProperty("webdriver.chrome.driver", driverPath);  
 // Create driver object for CHROME browser  
 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless");
-                chromeOptions.addArguments("--no-sandbox");
- 
-                WebDriver driver = new ChromeDriver(chromeOptions);  
+chromeOptions.addArguments("--headless");
+chromeOptions.addArguments("--no-sandbox");
+
+WebDriver driver = new ChromeDriver(chromeOptions); 
 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
 driver.manage().window().maximize();  
-driver.get(baseUrl);  
+driver.get(baseUrl1);  
 // get the current URL of the page  
-String URL= driver.getCurrentUrl();  
+String URL= driver.getCurrentUrl(); 
+String title1 = driver.getTitle();
+String ExpectedTitle1 = "Google";
+Assert.assertEquals(title1, ExpectedTitle1);
 System.out.print(URL);  
 //get the title of the page  
-String title = driver.getTitle();
-String ExpectedTitle = "Google";
-Assert.assertEquals(title, ExpectedTitle);
+                  
+System.out.println(title1); 
 driver.quit(); 
 }     
+@Test             
+public void twingo() {      
+// set the system property for Chrome driver      
+System.setProperty("webdriver.chrome.driver", driverPath);  
+// Create driver object for CHROME browser  
+ChromeOptions chromeOptions = new ChromeOptions();
+chromeOptions.addArguments("--headless");
+chromeOptions.addArguments("--no-sandbox");
+
+WebDriver driver = new ChromeDriver(chromeOptions); 
+driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
+driver.manage().window().maximize();  
+driver.get(baseUrl2);  
+// get the current URL of the page  
+String URL= driver.getCurrentUrl(); 
+String title2 = driver.getTitle();
+String ExpectedTitle2 = "Sign in to your account";
+Assert.assertEquals(title2, ExpectedTitle2);
+System.out.print(URL);  
+//get the title of the page  
+                  
+System.out.println(title2); 
+driver.quit(); 
+}
 @BeforeTest  
 public void beforeTest() {    
 System.out.println("before test");  
